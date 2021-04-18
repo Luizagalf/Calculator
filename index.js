045753483630af84dd1c966a18937e46d5e9e68c
Luizagalf <luizagalf@gmail.com>
2021-04-18 18:28:45 +0200

    Homework2

---
+++ index.js
@@ -0,0 +1,23 @@
+function Addition() {
+    let one = prompt("Enter the first number");
+    let two = prompt("Enter the first number");
+    alert(+one + +two);
+}
+
+function Subtraction() {
+    let one = prompt("Enter the first number");
+    let two = prompt("Enter the first number");
+    alert(one - two);
+}
+
+function Multiplication() {
+    let one = prompt("Enter the first number");
+    let two = prompt("Enter the first number");
+    alert(one * two);
+}
+
+function Division() {
+    let one = prompt("Enter the first number");
+    let two = prompt("Enter the first number");
+    alert(one / two);
+}
