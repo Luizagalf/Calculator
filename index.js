c2012cb6e606c23be39a44069f9544303ea67da5
Luizagalf <luizagalf@gmail.com>
2021-04-21 12:06:47 +0300

    Добавление isNaN

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
+let error = 'Error! Enter the numbers';
+
 function Addition() {
     let one = prompt("Enter the first number");
     let two = prompt("Enter the first number");
-    alert(+one + +two);
-}
-
+    let sum = +one + +two;
+    if (Number.isNaN(sum)) {
+        alert(error);
+        return NaN;
+    }
+    alert(sum);
+    }
+    
 function Subtraction() {
     let one = prompt("Enter the first number");
     let two = prompt("Enter the first number");
-    alert(one - two);
+    let sub = one - two;
+    if (Number.isNaN(sub)) {
+        alert(error);
+        return NaN;
+    }
+    alert(sub);
 }
 
 function Multiplication() {
     let one = prompt("Enter the first number");
     let two = prompt("Enter the first number");
-    alert(one * two);
+    let mul = one * two;
+    if (Number.isNaN(mul)) {
+        alert(error);
+        return NaN;
+    }
+    alert(mul);
 }
 
 function Division() {
     let one = prompt("Enter the first number");
     let two = prompt("Enter the first number");
-    alert(one / two);
+    let div = one / two;
+    if (Number.isNaN(div)) {
+        alert(error);
+        return NaN;
+    }
+    alert(div);
 }
